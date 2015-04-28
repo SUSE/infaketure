68ae73c96cd68d5789c59c86e5b438197e4cb914
Bo Maryniuk <bo@suse.de>
2015-04-28 12:48:23 +0200

    Class that generates fake hostnames out of the cracklib in Ubuntu style. :-)

---
+++ fakehostnames.py
@@ -0,0 +1,65 @@
+#
+# Fun toy. Use cracklib to generate hostnames. :-)
+# Author: BOFH (bo@suse.de)
+#
+
+import os
+import string
+import random
+import re
+
+
+class FakeNames(object):
+    CRACKLIB = "/usr/share/cracklib"
+
+    def __init__(self):
+        self._history = list()
+        self._crack_dict = list()
+        self._idx = 0
+
+        if os.path.exists(self.CRACKLIB):
+            self._unpack_cracklib()
+            self._stub = None
+        else:
+            self._stub = 'base'
+
+    def _unpack_cracklib(self):
+        """
+        Unpack cracklib, if installed.
+        """
+        rnum = re.compile(r"\d")
+        for line in os.popen("/usr/sbin/cracklib-unpacker {0}/pw_dict".format(self.CRACKLIB)):
+            line = rnum.sub("", line).strip()
+            if len(line) < 3 or len(line) > 12:
+                continue
+            self._crack_dict.append(line)
+
+    def ubuntify(self):
+        """
+        Msidling like a rock-n-roll! \m/
+        """
+        choice = string.lowercase[random.randint(0, len(string.lowercase) - 1)]
+        prefs = list()
+        posts = list()
+        for word in self._crack_dict:
+            if word[0] == choice:
+                if word[-1] == "y":
+                    prefs.append(word)
+                else:
+                    posts.append(word)
+        return prefs[random.randint(0, len(prefs) - 1)], posts[random.randint(0, len(posts) - 1)]
+
+
+    def __call__(self, *args, **kwargs):
+        """
+        Generate a name.
+        """
+        pattern = "{0}-{1}"
+        if self._stub:  # No crack lib around. :-(
+            self._idx += 1
+            return pattern.format(self._stub, self._idx - 1)
+
+        # Yay!
+        self._history.append(pattern.format(*self.ubuntify()))
+
+        return self._history[-1]
