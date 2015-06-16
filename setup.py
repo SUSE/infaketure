9351668fd76f9ba4a3c46882e75cc44581ca14b5
Bo Maryniuk <bo@suse.de>
2015-06-16 14:14:03 +0200

    Do not install binary scripts for packaging

--- setup.py
+++ setup.py
@@ -4,16 +4,22 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+import sys
+
+data_files = list()
+if '--no-bin' not in sys.argv:
+    data_files.append(('/usr/bin/', ['fakeregks.py', 'fakeregplot.py']))
+else:
+    sys.argv.pop(sys.argv.index("--no-bin"))
+data_files.append(('/etc/fakereg/', ['defaultconf/scenario.scn', 'defaultconf/fakeregplot.conf']))
+
 
 setup(name='SUSE Manager Scalability Probe',
       version='1.0',
       package_dir={'fakereg': 'fakereg'},
       package_data={'fakereg': ['defaultconf/*']},
       packages=['fakereg', ],
-      data_files=[
-          ('/usr/bin/', ['fakeregks.py', 'fakeregplot.py']),
-          ('/etc/fakereg/', ['defaultconf/scenario.scn', 'defaultconf/fakeregplot.conf']),
-      ],
+      data_files=data_files,
       author='bo',
       author_email='bo@suse.de',
       url='',
