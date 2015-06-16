c3842517b54c98a1156627552c8303ad7771f7bf
Bo Maryniuk <bo@suse.de>
2015-06-16 14:46:52 +0200

    Adapt setup for the library rename

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 __author__ = "bo"
 
+VERSION = "0.1"
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 import sys
 
+pkg_name = "infaketure"
 data_files = list()
 if '--no-bin' not in sys.argv:
-    data_files.append(('/usr/bin/', ['fakeregks.py', 'fakeregplot.py']))
+    data_files.append(('/usr/bin/', ['infaketure.py', 'infaketure-plot.py']))
 else:
     sys.argv.pop(sys.argv.index("--no-bin"))
-data_files.append(('/etc/fakereg/', ['defaultconf/scenario.scn', 'defaultconf/fakeregplot.conf']))
+data_files.append(('/etc/infaketure/', ['defaultconf/scenario.scn', 'defaultconf/plot.conf']))
 
 
 setup(name='SUSE Manager Scalability Probe',
-      version='1.0',
-      package_dir={'fakereg': 'fakereg'},
-      package_data={'fakereg': ['defaultconf/*']},
-      packages=['fakereg', ],
+      version=VERSION,
+      package_dir={pkg_name: pkg_name},
+      package_data={pkg_name: ['defaultconf/*']},
+      packages=[pkg_name, ],
       data_files=data_files,
-      author='bo',
+      author='Bo Maryniuk',
       author_email='bo@suse.de',
       url='',
       license='MIT',
