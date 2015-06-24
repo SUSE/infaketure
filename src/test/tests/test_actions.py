137ceea49605803fd01c99181196141351ccbd68
Bo Maryniuk <bo@suse.de>
2015-06-24 14:17:43 +0200

    Test action and dispatcher

--- src/test/tests/test_actions.py
+++ src/test/tests/test_actions.py
@@ -1,17 +1,37 @@
 __author__ = 'bo'
 
 import unittest
-import sys
+import uuid
 from infaketure.actions import Dispatcher
 from infaketure.actions import Action
 
 
 class TestActions(unittest.TestCase):
     def setUp(self):
+        """
+        Setup the actions test.
+
+        :return:
+        """
         self.dispatcher = Dispatcher(None, None)
 
-    def tearDown(self):
-        pass
+    def test_action_object(self):
+        """
+        Test action object.
+
+        :return:
+        """
+        value = uuid.uuid4().hex
+        action = Action()
+        action.one.two.three.four.five = value
+        self.assertEqual(action.one.two.three.four.five, value)
 
     def test_dispatcher(self):
-        self.assertEqual(True, False)
+        """
+        Test dispatcher object.
+
+        :return:
+        """
+        for action in ['reboot', 'rhnsd', 'script', 'scap', 'systemid',
+                       'errata', 'distupgrade', 'configfiles', 'packages']:
+            self.assertTrue(hasattr(self.dispatcher, action))
